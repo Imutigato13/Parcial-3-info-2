b1e043d44b300c5681fbb9e1580e232839bebc0b
MZ <mario.zambrano@udea.edu.co>
2024-05-12 15:51:46 -0500

    Archivos iniciales

--- clases.py
+++ clases.py
@@ -15,3 +15,21 @@ class Paciente:
         self.__edad = ''
         self.__id = ''
         self.__imagen = ''
+
+    def get_nombre(self):
+        return self.__nombre
+    def get_edad(self):
+        return self.__edad
+    def get_id(self):
+        return self.__id
+    def get_imagen(self):    
+        return self.__imagen
+    
+    def set_nombre(self, nombre):
+        self.__nombre = nombre
+    def set_edad(self, edad):
+        self.__edad = edad
+    def set_id(self, id):
+        self.__id = id
+    def set_imagen(self, imagen):
+        self.__imagen = imagen
