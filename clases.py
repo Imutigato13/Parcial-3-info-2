b7d252d50865f6b5ee6936f403942d4a47c0446b
MZ <mario.zambrano@udea.edu.co>
2024-05-12 22:52:18 -0500

    Acabada primera opcion del menu y se agrego un apartado de pruebas

--- clases.py
+++ clases.py
@@ -3,19 +3,58 @@ import matplotlib.pyplot as plt
 import os
 import cv2
 import pydicom
-
-class sistema:
+import dicom2nifti
+import nilearn
+import nilearn.plotting
+import random
+class archivador:
 
     def __init__(self):
-        self.__archivo = ''
-class Paciente(sistema):
+        self.__archivos = []
 
-    def __init__(self):
-        super().__init__()
-        self.__nombre = ''
-        self.__edad = 0
-        self.__id = 0
+    def extraer_archivos(self, ruta):
+        archivos = os.listdir(ruta)
+        for archivo in archivos:
+            if archivo.endswith('.dcm'): 
+                ruta_dicom = os.path.join(ruta, archivo)
+                dicom = pydicom.dcmread(ruta_dicom) 
+                self.__archivos.append(dicom)
+        return self.__archivos
+    def datos_paciente(self):
+        nombre = self.__archivos[0].PatientName
+        id = self.__archivos[0].PatientID
+        try:
+            edad = self.__archivos[0][ 0x0010 , 0x1010 ].value
+        except:
+            edad = 'No disponible'
+        return nombre, id, edad
+
+    def extraer_imagen(self,ruta_dicom):
+        precont = 0
+        for filename in os.listdir(r'D:\Programas Py\Informatica 2\Parcial 3 info 2\NIFTI'):
+            if filename.startswith('NIFTI'):
+                subcont = int(filename.split("_")[-1])
+                if precont < subcont:
+                    precont = subcont
+                elif precont >= subcont:
+                    cont = precont
+        cont = precont
+        cont +=1
+        ruta_NIFTI = r'D:\Programas Py\Informatica 2\Parcial 3 info 2\NIFTI\NIFTI' + f'_{cont}' 
+        os.makedirs(ruta_NIFTI, exist_ok=True)
+        dicom2nifti.convert_directory(ruta_dicom,ruta_NIFTI)
+        conversion_NIFTI = os.listdir(ruta_NIFTI)
+        ruta_imagen = os.path.join(ruta_NIFTI, conversion_NIFTI[0])
+        imagen = nilearn.image.load_img(ruta_imagen)
+        return imagen
+class Paciente():
+
+    def __init__(self, nombre, edad, id):
+        self.__nombre = nombre
+        self.__edad = edad
+        self.__id = id
         self.__imagen = ''
+        
 
     def get_nombre(self):
         return self.__nombre
@@ -26,3 +65,5 @@ class Paciente(sistema):
     def get_imagen(self):    
         return self.__imagen
     
+    def set_imagenes(self, imagen):
+        self.__imagen = imagen
--- main.py
+++ main.py
@@ -3,13 +3,21 @@ dic_data = {}
 dic_pac = {}
 
 def main():
-    print("Bienvenido al sistema de imagenes hospitalarias".center(90, "-"))
+    print("Bienvenido al sistema de imagenes hospitalarias\n".center(90, "-"))
     while True:
         menu_1 = int(input("1.Ingresar paciente\n2.Ingresar imagen JPG o PNG\n3.Transformacion de imagen DICOM\n4.Manipulacion de imagen JPG o PNG\n5.Salir\n:"))
         if menu_1 == 1:
-            paciente = Paciente()
-            dic_pac[paciente.get_id()] = paciente
-            print("Paciente ingresado con exito")
+            ruta = input(r"Ingrese la ruta de la carpeta del paciente: ")
+            ran_id = random.randint(1, 1000)
+            carpeta = archivador()
+            dicom = carpeta.extraer_archivos(ruta)
+            nombre, id, edad = carpeta.datos_paciente()
+            paciente = Paciente(nombre, edad, id)
+            imagenes = carpeta.extraer_imagen(ruta)
+            paciente.set_imagenes(imagenes)
+            dic_pac[ran_id] = paciente
+            dic_data[ran_id] = dicom
+            print("Paciente ingresado con exito con el siguiente ID: ", ran_id)
         elif menu_1 == 2:
             imagen = 1
             dic_data[imagen.get_id()] = imagen
